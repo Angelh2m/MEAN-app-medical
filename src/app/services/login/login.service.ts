de71f70bb24de94ae5c5bf3d710af459f3fa5cc5
Angel Hernandez <ahm@Angels-MacBook-Pro.local>
2018-08-22 16:54:43 -0400

    Http module cross domain

--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -15,7 +15,6 @@ export class LoginService {
   constructor(private _http: HttpClient) { }
 
   loginUser(user) {
-
     let url = URL_ENDPOINTS.aws;
 
     const body = new HttpParams()
@@ -24,23 +23,4 @@ export class LoginService {
 
     return this._http.post(url, body)
   }
-
-  // loginUser(payload: User): Observable<any> {
-  //   let url = URL_ENDPOINTS.login;
-
-  //   let data = {
-  //     email: 'Angelh3m@gmail.com',
-  //     password: '123',
-  //     name: 'Hello Requesr'
-  //   }
-  //   const body = new HttpParams()
-  //     .set('email', 'Angelh3m@gmail.com')
-  //     .set('password', '123')
-
-  //   return this._http.post(url, body, {
-  //     headers: new HttpHeaders({
-  //       'Content-Type': 'application/json',
-  //       'Accept': 'application/json'
-  //     })
-  //   }
 }
