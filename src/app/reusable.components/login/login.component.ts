8cd767240a5622eb7d1b7f4374ed3f29c5b590a1
Angel Hernandez <ahm@Angels-MacBook-Pro.local>
2018-08-23 17:25:37 -0400

    UI changes

--- src/app/reusable.components/login/login.component.ts
+++ src/app/reusable.components/login/login.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  formTitle: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onChangeState(state) {
+    console.log(state);
+    this.formTitle = state.replace();
+
   }
 
 }
